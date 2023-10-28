25582df7e66b07bd7e665017e3d03731f10c9e36
myrinja <myrinjau98@gmail.com>
2023-10-28 20:23:58 +0300

    update all

--- server/app.py
+++ server/app.py
@@ -46,7 +46,27 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
 
+        if plant:
+           
+            plant.is_in_stock = data['is_in_stock']
+            db.session.commit()
+            return make_response(plant.to_dict(), 200)
+        else:
+            return make_response({}, 404)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('', 204)
+        else:
+            return make_response({}, 404)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
